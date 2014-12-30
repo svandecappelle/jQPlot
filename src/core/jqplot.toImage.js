4acdfb915a696502fb0ca12134ee098b7b3f9cc1
svandecappelle <steeve.vandecappelle@opti-mix.fr>
2014-12-30 16:35:48 +0100

    allow export throught link

--- src/core/jqplot.toImage.js
+++ src/core/jqplot.toImage.js
@@ -337,14 +337,13 @@
         var imgData = $(this).jqplotToImageStr({});
         if (imgData) {
             var d = new Date().toISOString().slice(0, 19).replace(/-/g, "");
-            if($(this).find("#jqplot-tmp-download-link").length == 0){
-                $(this).append("<a id='jqplot-tmp-download-link' style='display:none;' href='" + imgData + "' download='chart-" + d + ".png"+"'></a>");
+            if($(this).find("#jqplot-download-link").length == 0){
+                $(this).append("<a id='jqplot-download-link' style='display:none;' href='" + imgData + "' download='chart-" + d + ".png"+"'></a>");
             }else{
-                $(this).find('#jqplot-tmp-download-link').attr("href", imgData).attr("download", 'chart-' + d + '.png');
+                $(this).find('#jqplot-download-link').attr("href", imgData).attr("download", 'chart-' + d + '.png');
             }
 
-            $("#jqplot-tmp-download-link").trigger("click")
-            // $("#jqplot-tmp-download-link").remove();
+            $("#jqplot-download-link").get(0).click();
         }
 
     };
