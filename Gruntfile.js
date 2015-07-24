24f6962b0a2f301b1a11520c8533f598391a5e15
Steeve Vandecappelle <me@mizore.fr>
2015-07-24 20:06:20 +0200

    adding css building files

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,13 +11,23 @@ module.exports = function(grunt) {
                 }
             })
         }
+    },
+    cssmin: {
+        target: {
+            files: grunt.file.expandMapping(['src/core/*.css', 'src/plugins/**/*.css'], 'dist/', {
+                rename: function(destBase, destPath) {
+                    return destBase+destPath.replace("src/", "").replace('.css', '.min.css');
+                }
+            })
+        }
     }
   });
 
   // Load the plugin that provides the "uglify" task.
   grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
 
   // Default task(s).
-  grunt.registerTask('default', ['uglify']);
+  grunt.registerTask('default', ['uglify', 'cssmin']);
 
 };
