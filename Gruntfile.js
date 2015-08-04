cb72f843b1c152a25f97c96ea8e9b3fa2a0b2280
Johan Bové <johan.bove@gmail.com>
2015-08-04 15:38:59 +0200

    Update gruntfile
    Code formatting

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,33 +1,36 @@
-module.exports = function(grunt) {
+module.exports = function (grunt) {
 
-  // Project configuration.
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-    uglify: {
-        min: {
-            files: grunt.file.expandMapping(['src/core/*.js', 'src/plugins/**/*.js'], 'dist/', {
-                rename: function(destBase, destPath) {
-                    return destBase+destPath.replace("src/", "").replace('.js', '.min.js');
-                }
-            })
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        uglify: {
+            min: {
+                files: grunt.file.expandMapping(['src/core/*.js', 'src/plugins/**/*.js'], 'dist/', {
+                    rename: function (destBase, destPath) {
+                        return destBase + destPath.replace("src/", "").replace('.js', '.min.js');
+                    }
+                })
+            }
+        },
+        cssmin: {
+            target: {
+                files: grunt.file.expandMapping(['src/core/*.css', 'src/plugins/**/*.css'], 'dist/', {
+                    rename: function (destBase, destPath) {
+                        return destBase + destPath.replace("src/", "").replace('.css', '.min.css');
+                    }
+                })
+            }
         }
-    },
-    cssmin: {
-        target: {
-            files: grunt.file.expandMapping(['src/core/*.css', 'src/plugins/**/*.css'], 'dist/', {
-                rename: function(destBase, destPath) {
-                    return destBase+destPath.replace("src/", "").replace('.css', '.min.css');
-                }
-            })
-        }
-    }
-  });
+    });
 
-  // Load the plugin that provides the "uglify" task.
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-cssmin');
+    // Load the plugin that provides the "uglify" task.
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
 
-  // Default task(s).
-  grunt.registerTask('default', ['uglify', 'cssmin']);
+    // Default task(s).
+    grunt.registerTask('default', [
+        'uglify',
+        'cssmin'
+    ]);
 
 };
