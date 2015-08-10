7d9b35df78521f0ff51bfc5c0ef8bf5aada33f68
Johan Bové <johan.bove@gmail.com>
2015-08-10 18:25:36 +0200

    Added categoryAxisRenderer test

--- test/core/test.js
+++ test/core/test.js
@@ -111,6 +111,7 @@ $(function () {
                 dataSeries.push([2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199]);
                 
                 chart = $.jqplot('chart', dataSeries, chartOptions);
+                
             });
             
             it("Simple construction", function () {
@@ -200,4 +201,35 @@ $(function () {
         });
         
     });
+    
+    describe("Plugins", function () {
+    
+        describe("Axis", function () {
+        
+            describe("categoryAxisRenderer", function () {
+            
+                beforeEach(function () {
+                
+                    dataSeries = [];
+                    
+                    chartOptions.axes.xaxis.renderer = $.jqplot.CategoryAxisRenderer;                    
+                    chartOptions.axes.xaxis.ticks = ["Cat A", "Cat B", "Cat C", "Cat D"];
+                    
+                    dataSeries.push([100, 0, -20, 53.5]);
+
+                    chart = $.jqplot('chart', dataSeries, chartOptions);
+
+                });
+
+                it("Simple construction", function () {
+                    should.exist(chart);
+                    console.log(chart);
+                });
+            
+            });
+            
+        });
+    
+    });
+    
 });
