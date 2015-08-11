c94806dac1051671162cb0de04d08ec759fdfc00
Johan Bové <johan.bove@gmail.com>
2015-08-11 14:57:27 +0200

    Fixed after merge

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,26 +1,39 @@
+/*global module */
 module.exports = function (grunt) {
 
-  // Project configuration.
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-    uglify: {
-        min: {
-            files: grunt.file.expandMapping(['src/core/*.js', 'src/plugins/**/*.js'], 'dist/', {
-                rename: function(destBase, destPath) {
-                    return destBase + destPath.replace("src/", "").replace('.js', '.min.js');
+    "use strict";
+    
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        uglify: {
+            min: {
+                files: grunt.file.expandMapping(['src/core/*.js', 'src/plugins/**/*.js'], 'dist/', {
+                    rename: function (destBase, destPath) {
+                        return destBase + destPath.replace("src/", "").replace('.js', '.min.js');
+                    }
+                })
+            },
+            dev: {
+                files: grunt.file.expandMapping(['src/core/*.js', 'src/plugins/**/*.js'], 'dist/', {
+                    rename: function (destBase, destPath) {
+                        return destBase + destPath.replace("src/", "").replace('.js', '.min.js');
+                    }
+                }),
+                options: {
+                    beautify: true,
+                    mangle: false,
+                    sourceMap: true
                 }
-            })
+            }
         },
-        dev: {
-            files: grunt.file.expandMapping(['src/core/*.js', 'src/plugins/**/*.js'], 'dist/', {
-                rename: function(destBase, destPath) {
-                    return destBase + destPath.replace("src/", "").replace('.js', '.min.js');
-                }
-            }),
-            options: {
-                beautify: true,
-                mangle: false,
-                sourceMap: true
+        cssmin: {
+            target: {
+                files: grunt.file.expandMapping(['src/core/*.css', 'src/plugins/**/*.css'], 'dist/', {
+                    rename: function (destBase, destPath) {
+                        return destBase + destPath.replace("src/", "").replace('.css', '.min.css');
+                    }
+                })
             }
         }
     });
